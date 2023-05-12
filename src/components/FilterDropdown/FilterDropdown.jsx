5ae7f2bdae9651dbf31243ab7bd7227deb9bc326
cappeloow <cappe1996@hotmail.com>
2023-05-12 14:58:23 +0200

    first phase done

---
+++ src/components/FilterDropdown/FilterDropdown.jsx
@@ -0,0 +1,11 @@
+export default function FilterDropdown() {
+    return (
+        <>
+       <select name="" id="">
+        <option value="">Popular</option>
+        <option value="">Top Rated</option>
+        <option value="">Upcoming</option>
+       </select>
+        </>
+    )
+}
