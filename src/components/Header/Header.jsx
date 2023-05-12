4e40ce232fc96c455daea4d6f350958d3eb9d2c5
cappeloow <cappe1996@hotmail.com>
2023-05-12 14:34:38 +0200

    rearranged my files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react"
-import NavLinks from "./NavLinks/NavLinks";
-import MyMovies from "./MyMovies/MyMovies"
+import NavLinks from "../NavLinks/NavLinks";
+import MyMovies from "../MyMovies/MyMovies";
 import './header.css';
 export default function Header(){
     return (
